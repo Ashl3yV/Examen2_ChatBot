68f8bcf0398b1e43807b06358b9498c14c2d52c4
Ashl3yV <127549843+Ashl3yV@users.noreply.github.com>
2023-06-17 13:45:28 -0600

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,19 @@
+const footer = ({ }) => {
+    return (
+
+        <div className="footer">
+            <footer>
+                <div className="container ">
+                    <p className="font-bold">Contactos</p>
+                    <a href="mailto:ashley.mendoza.vasquez@est.una.ac.cr"> Correo </a><br />
+                    <a href="https://www.linkedin.com/in/ashley-mendoza-317711274/"> Linked In </a>
+                    <p>Telefono</p>
+                    <a href="tel:+50684561642">+506 8456-1642</a>
+                </div>
+            </footer>
+        </div>
+
+    )
+}
+
+export default footer
---
+++ src/components/header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const Header = () => {
+  const router = useRouter();
+
+  const handleInicioLinkClick = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    router.push('/');
+  };
+
+  return (
+    <header className="header">
+      
+      <div className="logo">
+        <img src="/img/Ash.jpg" alt="foto"/>
+      </div>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="#" onClick={handleInicioLinkClick}>Inicio</a></li>
+          <li><Link href="#">Acerca de</Link></li>
+          <li><Link href="#">Contáctenos</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
